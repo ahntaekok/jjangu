6259e3d49fbbce75355dbe4c6a4784051527acd3
ahntaekok <atk0349@naver.com>
2023-07-28 12:45:27 +0900

    최종 세팅 완료

--- board/src/main/java/com/jjangu/board/controller/boardController.java
+++ board/src/main/java/com/jjangu/board/controller/boardController.java
@@ -2,9 +2,12 @@ package com.jjangu.board.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 @Controller
 public class boardController {
 
+    @ResponseBody
     @GetMapping("/")
     public String controller(){
         return "하이!";
